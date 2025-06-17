2b84d21a44e355c65d3447b1a122453deb8ed855
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-17 23:52:25 +0300

    Imported ChakraUI Snippets

---
+++ src/components/ui/provider.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
+import { ColorModeProvider } from "./color-mode";
+
+export function Provider(props) {
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <ThemeProvider>{props.children}</ThemeProvider>
+      <ColorModeProvider {...props} />
+    </ChakraProvider>
+  );
+}
