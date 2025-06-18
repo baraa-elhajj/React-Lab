e447f142e19dfbd5ee75f95d0fc0dd57558698a4
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-18 23:23:48 +0300

    Create PackingList.jsx

---
+++ src/apps/PackingList.jsx
@@ -0,0 +1,30 @@
+import { Text } from "@chakra-ui/react";
+
+function Item({ name, isPacked }) {
+  return isPacked ? (
+    <li className="item">{name} ✅</li>
+  ) : (
+    <li className="item">{name}</li>
+  );
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <Text textStyle="sm" fontWeight="light">
+        <b>Description</b>: Applying conditional rendering on items, to display
+        a ✅ next to the items having the prop isPacked set to true.
+      </Text>
+      <br />
+      <Text textStyle="xl" fontWeight="bold">
+        My Packing List:
+      </Text>
+      <ul>
+        <Item name="Book" isPacked={true} />
+        <Item name="Eraser" isPacked={false} />
+        <Item name="Pen" isPacked={true} />
+        <Item name="Ruler" isPacked={true} />
+      </ul>
+    </section>
+  );
+}
