69421dc3200b50650086e86fc5422d2a6adeb845
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-18 23:23:31 +0300

    Update Gallery.jsx
    enhanced styling with ChakraUI

--- src/apps/Gallery.jsx
+++ src/apps/Gallery.jsx
@@ -1,10 +1,17 @@
 import Profile from "./Profile";
+import { Text } from "@chakra-ui/react";
 
 export default function Gallery() {
   return (
     <section>
-      <p>Description: My first component..</p>
-      <h1>Amazing scientists</h1>
+      <Text textStyle="sm" fontWeight="light">
+        <b>Description</b>: Applying import/export keywords, to display a
+        component inside another.
+      </Text>
+      <br />
+      <Text textStyle="xl" fontWeight="bold">
+        Amazing scientists
+      </Text>
       <Profile />
       <Profile />
       <Profile />
