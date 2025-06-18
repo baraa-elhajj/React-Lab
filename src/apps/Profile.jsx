fd9342cf77e53a83e81a8940b37323104a544b08
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-18 04:56:50 +0300

    Create Profile.jsx

---
+++ src/apps/Profile.jsx
@@ -0,0 +1,10 @@
+export default function Profile() {
+  return (
+    <img
+      width="100"
+      height="100"
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
