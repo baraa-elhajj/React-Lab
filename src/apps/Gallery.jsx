c7f29e3453d7770b995124f96c03f8d7d46b3e01
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-18 04:56:52 +0300

    Create Gallery.jsx

---
+++ src/apps/Gallery.jsx
@@ -0,0 +1,13 @@
+import Profile from "./Profile";
+
+export default function Gallery() {
+  return (
+    <section>
+      <p>Description: My first component..</p>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
