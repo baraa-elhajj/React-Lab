cabf204948495a6efbe1fb8561a7c65d1d9c9f69
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-20 00:59:44 +0300

    Update People.jsx

--- src/apps/People.jsx
+++ src/apps/People.jsx
@@ -1,6 +1,6 @@
 import { Text } from "@chakra-ui/react";
 
-const people = [
+const peopleArray = [
   "Creola Katherine Johnson: mathematician",
   "Mario José Molina-Pasquel Henríquez: chemist",
   "Mohammad Abdus Salam: physicist",
@@ -8,8 +8,37 @@ const people = [
   "Subrahmanyan Chandrasekhar: astrophysicist",
 ];
 
+// to be used later
+const people = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+  },
+];
+
 export default function PeopleInfo() {
-  const peopleInfoList = people.map((person) => <li>{person}</li>);
+  const peopleInfoList = peopleArray.map((person) => <li>{person}</li>);
   return (
     <section>
       <Text textStyle="sm" fontWeight="light">
