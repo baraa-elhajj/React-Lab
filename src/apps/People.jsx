612d39fcb26e139f6cb9be9a12f8708584eaf7de
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-20 01:01:19 +0300

    Update People.jsx
    moved to data.jsx

--- src/apps/People.jsx
+++ src/apps/People.jsx
@@ -8,35 +8,6 @@ const peopleArray = [
   "Subrahmanyan Chandrasekhar: astrophysicist",
 ];
 
-// to be used later
-const people = [
-  {
-    id: 0,
-    name: "Creola Katherine Johnson",
-    profession: "mathematician",
-  },
-  {
-    id: 1,
-    name: "Mario José Molina-Pasquel Henríquez",
-    profession: "chemist",
-  },
-  {
-    id: 2,
-    name: "Mohammad Abdus Salam",
-    profession: "physicist",
-  },
-  {
-    id: 3,
-    name: "Percy Lavon Julian",
-    profession: "chemist",
-  },
-  {
-    id: 4,
-    name: "Subrahmanyan Chandrasekhar",
-    profession: "astrophysicist",
-  },
-];
-
 export default function PeopleInfo() {
   const peopleInfoList = peopleArray.map((person) => <li>{person}</li>);
   return (
