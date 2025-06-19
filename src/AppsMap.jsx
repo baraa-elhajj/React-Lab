e4933303b93614d52b039e01c3baee39d1682dfd
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-20 00:52:35 +0300

    Update AppsMap.jsx
    included People app in AppsMap

--- src/AppsMap.jsx
+++ src/AppsMap.jsx
@@ -1,9 +1,11 @@
 import Gallery from "./apps/Gallery";
 import PackingList from "./apps/PackingList";
+import PeopleInfo from "./apps/People";
 
 const appsMap = {
   Gallery: <Gallery />,
   "Packing List": <PackingList />,
+  "People Info": <PeopleInfo />,
 };
 
 export default appsMap;
