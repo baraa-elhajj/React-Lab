4450664c9c05d4baafbc50c77657a39ad4836902
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-20 01:17:03 +0300

    Update People.jsx
    display chemists list

--- src/apps/People.jsx
+++ src/apps/People.jsx
@@ -1,4 +1,6 @@
 import { Text } from "@chakra-ui/react";
+import { people } from "./data";
+import { getImageUrl } from "./utils";
 
 const peopleArray = [
   "Creola Katherine Johnson: mathematician",
@@ -7,6 +9,25 @@ const peopleArray = [
   "Percy Lavon Julian: chemist",
   "Subrahmanyan Chandrasekhar: astrophysicist",
 ];
+
+function Chemists() {
+  const chemistsList = people.filter(
+    (person) => person.profession === "chemist"
+  );
+
+  const result = chemistsList.map((person) => (
+    <li>
+      <img src={getImageUrl(person)} alt={person.name} />
+      <p>
+        <b>{person.name}:</b>
+        {" " + person.profession + " "}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  ));
+
+  return <ul>{result}</ul>;
+}
 
 export default function PeopleInfo() {
   const peopleInfoList = peopleArray.map((person) => <li>{person}</li>);
@@ -19,6 +40,12 @@ export default function PeopleInfo() {
       </Text>
       <br />
       <ul>{peopleInfoList}</ul>
+
+      <br />
+      <Text textStyle="xl" fontWeight="bold">
+        Chemists:
+      </Text>
+      <Chemists />
     </section>
   );
 }
