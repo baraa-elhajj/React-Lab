58f9c0399e02a10bf4a267d4b03a1d0104ce2042
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-22 16:50:31 +0300

    Update AppsMap.jsx

--- src/AppsMap.jsx
+++ src/AppsMap.jsx
@@ -1,11 +1,13 @@
 import Gallery from "./apps/Gallery";
 import PackingList from "./apps/PackingList";
 import PeopleInfo from "./apps/People";
+import RecipeList from "./apps/Recipes";
 
 const appsMap = {
   Gallery: <Gallery />,
   "Packing List": <PackingList />,
   "People Info": <PeopleInfo />,
+  Recipe: <RecipeList />,
 };
 
 export default appsMap;
