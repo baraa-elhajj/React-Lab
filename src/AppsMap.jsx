0094916cc65ff2a3689b9b5ec0b13bab39e4b112
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-26 01:54:42 +0300

    Update AppsMap.jsx
    include clock app in app map

--- src/AppsMap.jsx
+++ src/AppsMap.jsx
@@ -2,12 +2,14 @@ import Gallery from "./apps/Gallery";
 import PackingList from "./apps/PackingList";
 import PeopleInfo from "./apps/People";
 import RecipeList from "./apps/Recipes";
+import ClockApp from "./apps/Clock";
 
 const appsMap = {
   Gallery: <Gallery />,
   "Packing List": <PackingList />,
   "People Info": <PeopleInfo />,
   Recipe: <RecipeList />,
+  Clock: <ClockApp />,
 };
 
 export default appsMap;
