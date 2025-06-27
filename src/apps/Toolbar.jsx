2fbb093b905feb0aa73ea61f3d73b2a8e5b297e2
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-27 15:07:04 +0300

    Create Toolbar.jsx

---
+++ src/apps/Toolbar.jsx
@@ -0,0 +1,39 @@
+import { Button, HStack } from "@chakra-ui/react";
+import Header from "../components/ui/custom/Header";
+
+function CustomButton({ onClick, children }) {
+  return (
+    <Button onClick={onClick} size="xs" variant="surface">
+      {children}
+    </Button>
+  );
+}
+
+function PlayButton({ movieName }) {
+  function handlePlayClick() {
+    alert(`Playing ${movieName}!`);
+  }
+
+  return (
+    <CustomButton onClick={handlePlayClick}>Play "{movieName}"</CustomButton>
+  );
+}
+
+function PauseButton() {
+  return (
+    <CustomButton onClick={() => alert("Movie Paused")}>Pause</CustomButton>
+  );
+}
+
+export default function Toolbar() {
+  return (
+    <div>
+      <Header description="A simple toolbar app. Tested passing event handlers as props." />
+
+      <HStack wrap="wrap" gap="5">
+        <PlayButton movieName="Movie 1" />
+        <PauseButton />
+      </HStack>
+    </div>
+  );
+}
