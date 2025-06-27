f1ecb69eb2858348e560ddc85a50b4ccc13b8d38
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-28 00:21:19 +0300

    Update Restaurants.jsx
    Enhanced the page design using ChakraUI

--- src/apps/Restaurants.jsx
+++ src/apps/Restaurants.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { restaurantsList } from "./data";
+import { Image, Text, HStack, VStack, Button } from "@chakra-ui/react";
+import Header from "../components/ui/custom/Header";
 
 export default function Restaurants() {
   const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
+  const [showMore, setShowMore] = useState(true);
 
   function handleNextClick() {
     if (index === restaurantsList.length - 1) {
@@ -21,27 +23,49 @@ export default function Restaurants() {
     }
   }
 
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
-
   let restaurant = restaurantsList[index];
   return (
     <>
-      <button onClick={handlePreviousClick}>Previous</button>
-      <button onClick={handleNextClick}>Next</button>
-      <h2>
-        <i>{restaurant.name} </i>
-        by {restaurant.chef}
-      </h2>
-      <h3>
+      <Header description="Rendering a Restaurant Gallery app, focusing on React useState() hook." />
+
+      <Text textStyle="lg" fontWeight="bold">
+        {restaurant.name}
+      </Text>
+      <HStack wrap="wrap" gap="5">
+        <Image
+          src={restaurant.url}
+          boxSize="200px"
+          borderRadius="md"
+          fit="cover"
+          alt={restaurant.alt}
+        />
+        <VStack wrap="wrap" gap="2" alignItems="flex-start">
+          <Text textStyle="sm">Founded by:</Text>
+          <Text fontWeight="bold">{restaurant.chef}</Text>
+          <Text textStyle="sm" maxWidth="2/3">
+            {showMore && restaurant.description}
+          </Text>
+          <Button
+            onClick={() => setShowMore(!showMore)}
+            size="2xs"
+            variant="surface"
+          >
+            <Text textStyle="2xs">{showMore ? "Hide" : "Show"} details</Text>
+          </Button>
+        </VStack>
+      </HStack>
+      <Text textStyle="xs" fontWeight="light">
         ({index + 1} of {restaurantsList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? "Hide" : "Show"} details
-      </button>
-      {showMore && <p>{restaurant.description}</p>}
-      <img src={restaurant.url} alt={restaurant.alt} />
+      </Text>
+      <br />
+      <HStack wrap="wrap" gap="2">
+        <Button onClick={handlePreviousClick} size="2xs" variant="surface">
+          <Text textStyle="2xs">Previous</Text>
+        </Button>
+        <Button onClick={handleNextClick} size="2xs" variant="surface">
+          <Text textStyle="2xs">Next</Text>
+        </Button>
+      </HStack>
     </>
   );
 }
