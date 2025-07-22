11db791b46b9dd560f3f6a5693442e71185fe7e9
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-07-22 09:23:46 +0300

    Update AdvancedTodo.jsx
    included todo list component

--- src/apps/AdvancedTodo.jsx
+++ src/apps/AdvancedTodo.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
 import Header from "../components/ui/custom/Header";
 import { Text, CheckboxCard, VStack } from "@chakra-ui/react";
 import AddTodoForm from "../components/ui/custom/Todo/AddTodoForm";
+import TodoList from "../components/ui/custom/Todo/TodoList";
 
 export default function AdvancedTodo() {
-  const [todoList, setTodoList] = useState([]);
-
   return (
     <>
       <Header
@@ -15,42 +13,11 @@ export default function AdvancedTodo() {
         into one context provider file."
       />
 
-      {/* TODO: change the app logic to use a reducer/context combination. */}
-
       <Text textStyle="sm" fontWeight="bold">
         What's on your mind?
       </Text>
       <AddTodoForm />
-
       <br />
-
-      <VStack wrap="wrap" gap="2" alignItems="flex-start">
-        <Text textStyle="sm" fontWeight="bold">
-          TODO List:
-        </Text>
-        {todoList.length == 0 ? (
-          <Text textStyle="xs" fontStyle="italic">
-            Nothing to do yet, add something...
-          </Text>
-        ) : (
-          todoList.map((todo) => (
-            <CheckboxCard.Root
-              key={todo.id}
-              variant="subtle"
-              colorPalette="teal"
-              w="2xs"
-            >
-              <CheckboxCard.HiddenInput />
-              <CheckboxCard.Control>
-                <CheckboxCard.Label>
-                  <Text textStyle="sm">{todo.data}</Text>
-                </CheckboxCard.Label>
-                <CheckboxCard.Indicator />
-              </CheckboxCard.Control>
-            </CheckboxCard.Root>
-          ))
-        )}
-      </VStack>
-    </>
+      <TodoList />
   );
 }
