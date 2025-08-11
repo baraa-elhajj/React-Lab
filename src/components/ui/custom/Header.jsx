d4d9a0136756b7f93273820f300d6947363ca908
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-08-12 00:01:16 +0300

    Update Header.jsx
    added a description for the header component to easily understand the use of its props

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -2,6 +2,12 @@ import { Text } from "@chakra-ui/react";
 import Divider from "./Divider";
 import Badges from "./Badges";
 
+/**
+ *
+ * @param {string} props.description - Text to display in the header section.
+ * @param {number[]} props.badgesIds -  A list of badge identifiers. Each ID corresponds to a specific badge from data.jsx/reactBadges[].
+ * @returns Returns a paragraph containing the description provided, a list of tech badges, and a divider.
+ */
 export default function Header({ description, badgesIds }) {
   return (
     <>
